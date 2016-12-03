ngrewe/digitalocean_flocker_plugin

commit d21c3a97c46262da1cd549a31f94a1db3ad1b42d

Await node start

digitalocean_flocker_plugin/digitalocean_flocker_plugin.py
@@ -168,11 +168,14 @@ class DigitalOceanDeviceAPI(object):
         return not action or (action.status == 'in-progress' and
                               (time.time() - from_time < self._timeout))
 
-    def _await_action(self, action_id):
-        s = scheduler(time.time, time.sleep)
-        started_at = time.time()
+    def _await_action_id(self, action_id):
         action = self.Action.get_object(self._manager.token,
                                         action_id)
+        self._await_action(action)
+
+    def _await_action(self, action):
+        s = scheduler(time.time, time.sleep)
+        started_at = time.time()
         while self._should_wait_on(action, started_at):
             delta = max(0, min(self._poll,
                                self._timeout - (time.time() - started_at)))
@@ -259,7 +262,7 @@ class DigitalOceanDeviceAPI(object):
                                 attached_to=vol.droplet_ids[0])
                     r = vol.detach(vol.droplet_ids[0],
                                    vol.region['slug'])
-                    self._await_action(r['action']['id'])
+                    self._await_action_id(r['action']['id'])
 
                 vol.destroy()
             except NotFoundError as _:
@@ -275,7 +278,7 @@ class DigitalOceanDeviceAPI(object):
                 if vol.droplet_ids:
                     raise AlreadyAttachedVolume(blockdevice_id)
                 r = vol.attach(attach_to, vol.region["slug"])
-                if self._await_action(r['action']['id']):
+                if self._await_action_id(r['action']['id']):
                     vol.droplet_ids = [attach_to]
                 return self._to_block_device_volume(vol)
             except NotFoundError as _:
@@ -293,7 +296,7 @@ class DigitalOceanDeviceAPI(object):
                 region = vol.region["slug"]
                 r = vol.detach(detach_from, region)
 
-                if self._await_action(r['action']['id']):
+                if self._await_action_id(r['action']['id']):
                     vol.droplet_ids = None
                 a.add_success_fields(detached_from={
                     'droplet_id': detach_from,
@@ -335,8 +338,8 @@ class DigitalOceanDeviceAPI(object):
     def start_node(self, compute_instance_id):
         droplet = self._manager.get_droplet(compute_instance_id)
         if droplet.status != 'active':
-            droplet.power_on()
-
+            action = droplet.power_on(return_dict=False)
+            self._await_action(action)
 
 def do_from_configuration(cluster_id, token=None):
     return DigitalOceanDeviceAPI(cluster_id, token)

digitalocean_flocker_plugin/test/test_blockdevice.py
@@ -237,7 +237,8 @@ class TestBlockDeviceAPI(unittest.TestCase):
     @mock.patch('digitalocean.Volume', autospec=MockableVolume)
     @mock.patch.object(DigitalOceanDeviceAPI, 'Action', autospec=True)
     def test_destroy_volume(self, mock_action, mock_volume, mock_get_volume):
-        mock_get_volume.side_effect = lambda s, x: self._populate_volume(x, mock_volume())
+        mock_get_volume.side_effect = lambda s, x: \
+            self._populate_volume(x, mock_volume())
         mock_action.get_object.status = 'completed'
         self._api.destroy_volume('1235')
         self.assertEqual(1, mock_volume().destroy.call_count,
@@ -263,8 +264,8 @@ class TestBlockDeviceAPI(unittest.TestCase):
     def test_destroy_volume_attached(self, mock_volume, mock_action,
                                      mock_get_volume):
         mock_action.get_object.status = 'completed'
-        mock_get_volume.side_effect = lambda s, x: self._populate_volume(x,
-                                                                mock_volume())
+        mock_get_volume.side_effect = lambda s, x: \
+            self._populate_volume(x, mock_volume())
         self._api.destroy_volume('1234')
         self.assertEqual(1, mock_volume().detach.call_count,
                          'volume detached prior to destruction')
@@ -371,7 +372,10 @@ class TestCloudAPI(unittest.TestCase):
 
     @mock.patch.object(Manager, 'get_droplet', autospec=True)
     @mock.patch('digitalocean.Droplet', autospec=MockableDroplet)
-    def test_start_node(self, mock_droplet, mock_get_droplet):
+    @mock.patch.object(DigitalOceanDeviceAPI, 'Action', autospec=True)
+    def test_start_node(self, mock_action, mock_droplet, mock_get_droplet):
+        mock_action().status = 'completed'
+        mock_droplet().power_on.return_value = mock_action()
         mock_get_droplet.side_effect = lambda s, x: \
             self._populate_droplet(x, mock_droplet())
         self._api.start_node(six.text_type('32'))

commit dc0f96513016d494d2c3a0a21147f5cf2e087e49

Fix a silly bug introduced in d21c3a9
Also improve test coverage for awaiting state changes.

digitalocean_flocker_plugin/digitalocean_flocker_plugin.py
@@ -58,8 +58,8 @@ class DigitalOceanDeviceAPI(object):
 
     _PREFIX = six.text_type("flocker-v1-")
 
-    # We reassign the Volume class as an attribute to help ergonomics in our
-    # test suite.
+    # We reassign the Volume and Action class as attributes to help
+    # ergonomics in our test suite.
     Volume = Vol
     Action = Act
 
@@ -168,19 +168,12 @@ class DigitalOceanDeviceAPI(object):
             result_dict["okay"].append(vol)
         return result_dict
 
-    def _should_wait_on(self, action, from_time):
-        return not action or (action.status == 'in-progress' and
-                              (time.time() - from_time < self._timeout))
-
     def _await_action_id(self, action_id):
         action = self.Action.get_object(self._manager.token,
                                         action_id)
-        self._await_action(action)
+        return self._await_action(action)
 
     def _await_action(self, action):
-        s = scheduler(time.time, time.sleep)
-        i = 0
-        started_at = time.time()
         if action and action.status == 'completed':
             return True
         elif not action:
@@ -188,12 +181,10 @@ class DigitalOceanDeviceAPI(object):
         with start_action(action_type=six.text_type(
                 'flocker:node:agents:do:await'), do_action_type=action.type,
                           do_action_id=action.id) as ac:
-            while self._should_wait_on(action, started_at):
-                delta = max(0, min(self._poll,
-                                   self._timeout - (time.time() - started_at)))
-                s.enter(delta, 0, lambda x: x.load_directly(), (action,))
-                s.run()
-                i += 1
+            i = self._iterations_until(lambda x: not x or x.status !=
+                                       'in-progress',
+                                       lambda x: x.load_directly(), (action,))
+
             if action.status == 'completed':
                 ac.add_success_fields(iterations=i,
                                       do_action_status='completed')
@@ -212,6 +203,34 @@ class DigitalOceanDeviceAPI(object):
                                       format(r=action.status))
 
         return action and action.status == 'completed'
+
+    def _iterations_until(self, completed, update_state, argument):
+        """ Poll for a state change to complete callable-s
+
+        :param completed: A callable accepting argument, returning true if the
+        state change has successfully completed.
+        :param update_state: The action to execute in order to poll for a state
+        change
+        :param argument: The arguments on which to execute both the check and
+        the action. Probably a tuple.
+        :return: The number of iterations taken
+        """
+
+        if completed(*argument):
+            return 0
+        s = scheduler(time.time, time.sleep)
+        i = 0
+        started_at = time.time()
+        while not completed(*argument) and not self._has_timed_out(started_at):
+            delta = max(0, min(self._poll,
+                               self._timeout - (time.time() - started_at)))
+            s.enter(delta, 0, update_state, argument)
+            s.run()
+            i += 1
+        return i
+
+    def _has_timed_out(self, from_time):
+        return time.time() - from_time >= self._timeout
 
     def list_volumes(self):
         with start_action(action_type=six.text_type(

digitalocean_flocker_plugin/functional_tests.py
@@ -10,18 +10,21 @@ import six
 def do_blockdeviceapi_for_test(test_case):
     return get_blockdeviceapi_with_cleanup(test_case)
 
+
 MIN_ALLOCATION_SIZE = GiB(1).to_Byte().value
 
 MIN_ALLOCATION_UNIT = GiB(1).to_Byte().value
 
 
-class DigitialOceanBlockDeviceAPITests(
+class DigitalOceanBlockDeviceAPITests(
     make_iblockdeviceapi_tests(
         blockdevice_api_factory=do_blockdeviceapi_for_test,
         unknown_blockdevice_id_factory=lambda x: six.text_type(2147483647))):
-    """Functional tests for ``IBlockDeviceAPI`` implementation for DigitalOcean"""
+    """Functional tests for DigitalOcean ``IBlockDeviceAPI`` implementation
+        """
 
 
-class DigitalOceanCloudeAPITests(
-    make_icloudapi_tests(blockdevice_api_factory=do_blockdeviceapi_for_test)):
-    """Functional tests for ``ICloudAPI`` implementation for DigitalOcean"""
+class DigitalOceanCloudAPITests(
+    make_icloudapi_tests(
+        blockdevice_api_factory=do_blockdeviceapi_for_test)):
+    """Functional tests for DigitalOcean ``ICloudAPI`` implementation"""

digitalocean_flocker_plugin/test/test_blockdevice.py
@@ -314,6 +314,30 @@ class TestBlockDeviceAPI(unittest.TestCase):
             six.text_type('flocker-v1-0ff663594f6347c8a950ff5de6f6225e')),
                          self._api.get_device_path(six.text_type('1234')))
 
+    def test_polling(self):
+
+        class Arg(object):
+
+            def __init__(self):
+                self.count = 0
+
+            def poll(self):
+                self.count += 3
+
+            def done(self):
+                return self.count == 3
+
+        arg = Arg()
+        old_poll = self._api._poll
+        try:
+            self._api._poll = 0.001
+            i = self._api._iterations_until(lambda x: x.done(),
+                                            lambda x: x.poll(),
+                                            (arg,))
+            self.assertEqual(1, i, 'polled once')
+        finally:
+            self._poll = old_poll
+
     def tearDown(self):
         self._api = None
         self._cluster_id = None